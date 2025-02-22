52fa93744ba72464c2f1d462f998d29230030f5e
RootSec <mortalsoft23@gmail.com>
2025-02-22 23:32:58 +0100

    BANNER

--- core/classes/UserManager.class.php
+++ core/classes/UserManager.class.php
@@ -1,4 +1,12 @@
 <?php
+/************************************************
+ *  InfernoFramework  
+ *  https://github.com/InfernoVM/InfernoFramework  
+ *  
+ *  Powered by InfernoVM  
+ *  Fast, Secure, and Reliable PHP Framework  
+ ************************************************/
+
 if (!defined('rootsec')) {
     die('Direct access not permitted');
 }
